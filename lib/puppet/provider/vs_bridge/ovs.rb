f5e682df785872a3901cac2d1b723d778c90a3fd
Endre Karlson <endre.karlson@gmail.com>
2012-10-06 21:28:31 +0200

    Permanently fix support for External IDs

--- lib/puppet/provider/vs_bridge/ovs.rb
+++ lib/puppet/provider/vs_bridge/ovs.rb
@@ -17,19 +17,23 @@ Puppet::Type.type(:vs_bridge).provide(:ovs) do
         vsctl("del-br", @resource[:name])
     end
 
-    def _split(string, splitter="\n")
+    def _split(string, splitter=",")
         return Hash[string.split(splitter).map{|i| i.split("=")}]
     end
 
     def external_ids
         result = vsctl("br-get-external-id", @resource[:name])
-        return _split result
+        return result.split("\n").join(",")
     end
 
     def external_ids=(value)
-        ids = _split(value, ",")
-        ids.each_pair do |k,v|
-            vsctl("br-set-external-id", @resource[:name], k, v)
+        old_ids = _split(external_ids)
+        new_ids = _split(value)
+
+        new_ids.each_pair do |k,v|
+            unless old_ids.has_key?(k)
+                vsctl("br-set-external-id", @resource[:name], k, v)
+            end
         end
     end
 end
