65461ecaa718abc8e7d0ecfb2c315f3f751c4a06
Endre Karlson <endre.karlson@gmail.com>
2012-10-06 22:12:33 +0200

    Make it provider ish

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,6 @@
-class vswitch {
-    package { ["openvswitch-switch", "openvswitch-datapath-dkms"]:
-        ensure => present
-    }
-
-    service { "openvswitch-switch":
-        ensure      => true,
-        enable      => true,
-        hasstatus  => true,
-        status      => "/etc/init.d/openvswitch-switch",
-        require     => Package["openvswitch-switch"]
-    }
+class vswitch (
+  $provider = "ovs"
+) {
+  $cls = "vswitch::$provider"
+  include $cls
 }
---
+++ manifests/ovs.pp
@@ -0,0 +1,17 @@
+class vswitch::ovs {
+  case $::osfamily {
+    Debian: {
+      package {["openvswitch-switch", "openvswitch-datapath-dkms"]: ensure => present}
+    }
+    Ubuntu: {
+      package {["openvswitch-switch", "openvswitch-datapath-dkms"]: ensure => present}
+
+      service {"openvswitch-switch":
+        ensure      => true,
+        enable      => true,
+        hasstatus   => true,
+        status      => "/etc/init.d/openvswitch-switch",
+      }
+    }
+  }
+}
