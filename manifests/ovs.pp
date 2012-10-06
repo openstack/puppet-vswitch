51522bd1b79e4f0786c724712d924eaf928750df
root <root@puppet-test.dev.lan>
2012-10-06 21:44:02 +0000

    Code originally from OS Quantum

--- manifests/ovs.pp
+++ manifests/ovs.pp
@@ -2,6 +2,15 @@ class vswitch::ovs {
   case $::osfamily {
     Debian: {
       package {["openvswitch-switch", "openvswitch-datapath-dkms"]: ensure => present}
+
+      service { 'ovsdb-server':
+        name      => $::quantum::params::ovs_service,
+        enable    => true,
+        ensure    => running,
+        hasstatus => false,
+        status    => 'pgrep ovsdb-server',
+        require   => Package['openvswitch-datapath-dkms'],
+      }
     }
     Ubuntu: {
       package {["openvswitch-switch", "openvswitch-datapath-dkms"]: ensure => present}
