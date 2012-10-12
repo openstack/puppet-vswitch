9a4423f52af59fd6214bcc24304f164612c28b8a
Dan Bode <dan@puppetlabs.com>
2012-10-12 10:24:23 -0700

    Refactor vswitch::ovs
    * remove invalid Ubuntu osfamily
    * add package dep linux-headers-generic (may need
    to remove the version)
    * ensure packages are installed before service starts

--- manifests/ovs.pp
+++ manifests/ovs.pp
@@ -1,15 +1,17 @@
-class vswitch::ovs {
+class vswitch::ovs(
+  $package_ensure = 'present'
+) {
   case $::osfamily {
     Debian: {
+      ensure_resource(
+        'package',
+        'linux-headers-3.2.0-23-generic',
+        {'ensure' => 'present' }
+      )
       package {["openvswitch-switch", "openvswitch-datapath-dkms"]:
-        ensure => present,
-        before => Service['openvswitch-switch'],
-      }
-    }
-    Ubuntu: {
-      package {["openvswitch-switch", "openvswitch-datapath-dkms"]:
-        ensure => present,
-        before => Service['openvswitch-switch'],
+        ensure  => $package_ensure,
+        require => Package['linux-headers-3.2.0-23-generic'],
+        before  => Service['openvswitch-switch'],
       }
     }
   }
