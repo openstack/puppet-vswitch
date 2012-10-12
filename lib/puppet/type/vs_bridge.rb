640f49697a3d8fb4a616de02ce7a278ee96ff3f4
Dan Bode <dan@puppetlabs.com>
2012-10-11 22:40:04 -0700

    change spacing to 2 space
    to conform with Ruby/Puppet standards

--- lib/puppet/type/vs_bridge.rb
+++ lib/puppet/type/vs_bridge.rb
@@ -1,18 +1,19 @@
 require "puppet"
 
 module Puppet
-    Puppet::Type.newtype(:vs_bridge) do
-        @doc = "A Switch - For example 'br-int' in OpenStack"
+  Puppet::Type.newtype(:vs_bridge) do
+    @doc = "A Switch - For example 'br-int' in OpenStack"
 
-        ensurable
+    ensurable
 
-        newparam(:name) do
-            isnamevar
-            desc "The bridge to configure"
-        end
-
-        newproperty(:external_ids) do
-            desc "External IDs for the bridge"
-        end
+    newparam(:name) do
+      isnamevar
+      desc "The bridge to configure"
     end
+
+    newproperty(:external_ids) do
+      desc "External IDs for the bridge"
+    end
+
+  end
 end
--- lib/puppet/type/vs_port.rb
+++ lib/puppet/type/vs_port.rb
@@ -1,19 +1,19 @@
 require "puppet"
 
 module Puppet
-    Puppet::Type.newtype(:vs_port) do
-        @doc = "A Virtual Switch Port"
+  Puppet::Type.newtype(:vs_port) do
+    @doc = "A Virtual Switch Port"
 
-        ensurable
+    ensurable
 
-        newparam(:interface) do
-            isnamevar
-            desc "The interface to attach to the bridge"
-        end
-
-        newparam(:bridge) do
-            desc "What bridge to use"
-        end
+    newparam(:interface) do
+      isnamevar
+      desc "The interface to attach to the bridge"
     end
+
+    newparam(:bridge) do
+      desc "What bridge to use"
+    end
+  end
 end
 
