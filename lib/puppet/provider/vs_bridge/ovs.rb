e58877d037e8995fa4d87acce77821f7e0dc71ff
Dan Bode <dan@puppetlabs.com>
2012-10-12 10:27:22 -0700

    Switch to use 2 space indentation
    the standard for Ruby

--- lib/puppet/provider/vs_bridge/ovs.rb
+++ lib/puppet/provider/vs_bridge/ovs.rb
@@ -1,39 +1,39 @@
 require "puppet"
 
 Puppet::Type.type(:vs_bridge).provide(:ovs) do
-    commands :vsctl => "/usr/bin/ovs-vsctl"
+  commands :vsctl => "/usr/bin/ovs-vsctl"
 
-    def exists?
-        vsctl("br-exists", @resource[:name])
-    rescue Puppet::ExecutionFailure
-        return false
+  def exists?
+    vsctl("br-exists", @resource[:name])
+  rescue Puppet::ExecutionFailure
+    return false
+  end
+
+  def create
+    vsctl("add-br", @resource[:name])
+  end
+
+  def destroy
+    vsctl("del-br", @resource[:name])
+  end
+
+  def _split(string, splitter=",")
+    return Hash[string.split(splitter).map{|i| i.split("=")}]
+  end
+
+  def external_ids
+    result = vsctl("br-get-external-id", @resource[:name])
+    return result.split("\n").join(",")
+  end
+
+  def external_ids=(value)
+    old_ids = _split(external_ids)
+    new_ids = _split(value)
+
+    new_ids.each_pair do |k,v|
+      unless old_ids.has_key?(k)
+        vsctl("br-set-external-id", @resource[:name], k, v)
+      end
     end
-
-    def create
-        vsctl("add-br", @resource[:name])
-    end
-
-    def destroy
-        vsctl("del-br", @resource[:name])
-    end
-
-    def _split(string, splitter=",")
-        return Hash[string.split(splitter).map{|i| i.split("=")}]
-    end
-
-    def external_ids
-        result = vsctl("br-get-external-id", @resource[:name])
-        return result.split("\n").join(",")
-    end
-
-    def external_ids=(value)
-        old_ids = _split(external_ids)
-        new_ids = _split(value)
-
-        new_ids.each_pair do |k,v|
-            unless old_ids.has_key?(k)
-                vsctl("br-set-external-id", @resource[:name], k, v)
-            end
-        end
-    end
+  end
 end
