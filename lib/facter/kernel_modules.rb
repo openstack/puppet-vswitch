48281cb8deb6469d6aa0ac7d2a35cf4ac8f86427
Greg Dallavalle <greg.dallavalle@wolfnet.com>
2012-10-17 12:45:44 -0500

    Move get_modules inside setcode

--- lib/facter/kernel_modules.rb
+++ lib/facter/kernel_modules.rb
@@ -10,16 +10,13 @@ require "facter"
 require "set"
 
 def get_modules
-    if File.exists?("/proc/modules")
-        return File.readlines("/proc/modules").inject(Set.new){|s,l|s << l[/\w+\b/] }
-    end
+    return File.readlines("/proc/modules").inject(Set.new){|s,l|s << l[/\w+\b/]}
 end
-
-modules = get_modules
 
 Facter.add("kernel_modules") do
     confine :kernel => :linux
     setcode do
+        modules = get_modules
         modules.to_a.join(",")
     end
 end
