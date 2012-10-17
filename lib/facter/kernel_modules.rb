ecdcfe6b8b44fcb924072cd4837edaaf7d859f5d
Greg Dallavalle <greg.dallavalle@wolfnet.com>
2012-10-17 12:33:21 -0500

    Only read /proc/modules if it exists.  Confine the fact to Linux kernels.

--- lib/facter/kernel_modules.rb
+++ lib/facter/kernel_modules.rb
@@ -10,12 +10,15 @@ require "facter"
 require "set"
 
 def get_modules
-    return File.readlines("/proc/modules").inject(Set.new){|s,l|s << l[/\w+\b/] }
+    if File.exists?("/proc/modules")
+        return File.readlines("/proc/modules").inject(Set.new){|s,l|s << l[/\w+\b/] }
+    end
 end
 
 modules = get_modules
 
 Facter.add("kernel_modules") do
+    confine :kernel => :linux
     setcode do
         modules.to_a.join(",")
     end
