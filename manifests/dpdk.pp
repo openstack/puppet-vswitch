3d91514806a1bb32aa2ac0fed90ced23162ccc32
Saravanan KR <skramaja@redhat.com>
2017-12-06 17:20:58 +0530

    Remvoved OvS2.5 specific changes as it is no longer required
    In pike release, we have migrated manifests from ovs2.5 to ovs2.6 and
    retained the backward compatibility. Removing the ovs2.5 changes
    in this cycle as it is no longer used.
    
    Change-Id: I286b2f4d090dda34ec95f24f77baefcb7687ff85

--- manifests/dpdk.pp
+++ manifests/dpdk.pp
@@ -6,11 +6,6 @@
 # [*memory_channels*]
 #   (optional) The number of memory channels to use as an integer.
 #
-# [*driver_type*]
-#   (Optional) The DPDK Driver type
-#   Defaults to 'vfio-pci'
-#   This parameter is required only for OVS versions <= 2.5.
-#
 # [*host_core_list*]
 #   (optional) The list of cores to be used by the lcore threads.
 #   The host_core_list is a string with format as <c1>[-c2][,c3[-c4],...]
@@ -34,13 +29,21 @@
 #   socket 1 and no allocation for socket 0, the value should be "0,1024"
 #   Defaults to undef.
 #
+# DEPRECATED PARAMETERS
+#
+# [*driver_type*]
+#   (Optional) The DPDK Driver type
+#   Defaults to 'vfio-pci'
+#   This parameter is required only for OVS versions <= 2.5.
+#
 class vswitch::dpdk (
   $memory_channels    = undef,
-  $driver_type        = 'vfio-pci',
   $host_core_list     = undef,
   $package_ensure     = 'present',
   $pmd_core_list      = undef,
   $socket_mem         = undef,
+  # DEPRECATED PARAMETERS
+  $driver_type        = 'vfio-pci',
 ) {
 
   include ::vswitch::params
@@ -56,37 +59,6 @@ class vswitch::dpdk (
     tag    => 'openvswitch',
   }
 
-  # DEPRECATED support for OVS 2.5
-  # DPDK_OPTIONS is no longer used in ovs 2.6, since it was a distribution
-  # specific hack to the ovs-ctl scripts. Instead dpdk information is
-  # pulled from the ovsdb.
-  if $socket_mem and !empty($socket_mem) {
-    unless $socket_mem =~ /^(\d+\,?)+$/ {
-      fail( 'socket_mem is in incorrect format')
-    }
-    $socket_string = "--socket-mem ${socket_mem}"
-  }
-  else {
-    $socket_string = undef
-  }
-  if $driver_type {
-    $pci_list = inline_template('<%= Facter.value("pci_address_driver_#@driver_type") %>')
-    if empty($pci_list) {
-      $white_list = undef
-    }
-    else {
-      $white_list = inline_template('-w <%= @pci_list.gsub(",", " -w ") %>')
-    }
-  }
-  $options = "DPDK_OPTIONS = \"-l ${host_core_list} -n ${memory_channels} ${socket_string} ${white_list}\""
-  file_line { '/etc/sysconfig/openvswitch':
-    path    => '/etc/sysconfig/openvswitch',
-    match   => '^DPDK_OPTIONS.*',
-    line    => $options,
-    require => Package[$::vswitch::params::ovs_dpdk_package_name],
-    before  => Service['openvswitch'],
-  }
-
   $pmd_core_mask = range_to_mask($pmd_core_list)
   $dpdk_lcore_mask = range_to_mask($host_core_list)
 
@@ -98,9 +70,9 @@ class vswitch::dpdk (
   }
 
   $dpdk_configs = {
-    'other_config:dpdk-extra'      => { value => $memory_channels_conf, skip_if_version => '2.5'},
-    'other_config:dpdk-socket-mem' => { value => $socket_mem, skip_if_version => '2.5'},
-    'other_config:dpdk-lcore-mask' => { value => $dpdk_lcore_mask, skip_if_version => '2.5'},
+    'other_config:dpdk-extra'      => { value => $memory_channels_conf},
+    'other_config:dpdk-socket-mem' => { value => $socket_mem},
+    'other_config:dpdk-lcore-mask' => { value => $dpdk_lcore_mask},
     'other_config:pmd-cpu-mask'    => { value => $pmd_core_mask},
   }
 
@@ -111,10 +83,9 @@ class vswitch::dpdk (
   }
 
   vs_config { 'other_config:dpdk-init':
-    value           => 'true',
-    skip_if_version => '2.5',
-    require         => Service['openvswitch'],
-    wait            => true,
+    value   => 'true',
+    require => Service['openvswitch'],
+    wait    => true,
   }
 
   service { 'openvswitch':
