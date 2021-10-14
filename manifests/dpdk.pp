1fe71cf5e359c8f83f11b2673620be822423bf47
Takashi Kajinami <tkajinam@redhat.com>
2021-10-14 17:28:41 +0900

    Do not use service resource to restart openvswitch service
    Closes-Bug: #1946985
    Change-Id: Idac29b0ea6794a52ae9146da8105ebbaf65ee9a3

--- manifests/dpdk.pp
+++ manifests/dpdk.pp
@@ -120,9 +120,9 @@ class vswitch::dpdk (
   # lint:ignore:quoted_booleans
   if $enable_hw_offload {
     vs_config { 'other_config:hw-offload':
-      value  => 'true',
-      notify => Service['openvswitch'],
-      wait   => true,
+      value   => 'true',
+      restart => true,
+      wait    => true,
     }
   }
   # lint:endignore
@@ -153,6 +153,14 @@ class vswitch::dpdk (
     ensure => true,
     enable => true,
     name   => $::vswitch::params::ovs_service_name,
+  }
+
+  # NOTE(tkajinam): This resource is defined to restart the openvswitch services
+  # when any vs_config resource with restart => true is enabled.
+  exec { 'restart openvswitch':
+    path        => ['/sbin', '/usr/sbin', '/bin', '/usr/bin'],
+    command     => "systemctl -q restart ${::vswitch::params::ovs_service_name}.service",
+    refreshonly => true,
   }
 
   Service['openvswitch'] -> Vs_config<||>
